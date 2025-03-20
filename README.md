2e1194be67acb0b3a36381f06335504fda7ae9fd
NicolasMp3 <164546658+NicolasMp3@users.noreply.github.com>
2025-03-20 19:29:10 +0000

    ferramentas

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # back-end-tf-web
 “Back-End do trabalho final da disciplina de WEB”
+
+Grupo:
+Nicolas Morais & Pedro H. Rocha
+
+Ferramentas Utilizadas:
+- github.com
+- codespace
+- vercel.com
+- neontech.com
+- Uploadcare.com
+- brModelo
---
+++ src/db/DDL.sql
@@ -0,0 +1,13 @@
+CREATE TABLE usuarios (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    senha TEXT NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE imagens (
+    id SERIAL PRIMARY KEY,
+    usuario_id INT NOT NULL,
+    descricao TEXT,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
+);
